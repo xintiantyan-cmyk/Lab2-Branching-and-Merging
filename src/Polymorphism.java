af7cd39772aa764dfe02224857c2eb8976d06e29
Tina Yan <tinayan@Tinas-MacBook-Air-5.local>
2026-05-14 19:23:44 -0400

    task 1 done

--- src/Polymorphism.java
+++ src/Polymorphism.java
@@ -1,5 +1,4 @@
 public class Polymorphism {
-    // TODO TASK 1: Fix the bugs in this code to pass PolymorphismTest.java
 
     /**
      * Returns the role.
@@ -7,8 +6,8 @@ public class Polymorphism {
      * @return the role of the person
      */
     public static String getStudentRole(Person p) {
-        // TODO: right now for student, it is not returning the role to be student.
-        return p.role;
+        Student q = (Student) p;
+        return q.role;
     }
 }
 
